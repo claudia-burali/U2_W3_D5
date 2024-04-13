13bfbe6ea12283137bb7b1f0aa3f7a21bac5949f
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-04-13 18:12:51 +0200

    mod_1

--- backoffice.js
+++ backoffice.js
@@ -1,8 +1,53 @@
-const URL = "https://striveschool-api.herokuapp.com/api/product/";
+const p = new URLSearchParams(window.location.search);
+const id = p.get("appId");
+const URL = id
+  ? "https://striveschool-api.herokuapp.com/api/product/" + id
+  : "https://striveschool-api.herokuapp.com/api/product/";
+const met = id ? "PUT" : "POST";
 
 window.onload = () => {
   const form = document.querySelector("form");
   form.addEventListener("submit", handleSubmit);
+  const submitBtn = document.querySelector("button[type='submit']");
+  const subtitle = document.getElementById("subtitle");
+
+  if (id) {
+    subtitle.innerText = "Edit product spec";
+    submitBtn.classList.remove("btn-primary");
+    submitBtn.classList.add("btn-success");
+    submitBtn.innerText = "Edit";
+
+    const delBtn = document.querySelector(".btn-danger");
+    delBtn.addEventListener("click", handleDelete);
+    delBtn.classList.remove("d-none");
+
+    fetch(URL, {
+      method: met,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZTAzYjdmMzA0NjAwMWFlNTlmNTciLCJpYXQiOjE3MTI5MDYyOTksImV4cCI6MTcxNDExNTg5OX0._AqNCIr-c6jwkia_xiX-dt4Xe8vpunRnzSRUA6lp_E0",
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          throw new Error("Fetch error");
+        }
+      })
+      .then((prodToModify) => {
+        const { name, description, brand, imageUrl, price } = prodToModify;
+        document.getElementById("name").value = name;
+        document.getElementById("description").value = description;
+        document.getElementById("brand").value = brand;
+        document.getElementById("image").value = imageUrl;
+        document.getElementById("price").value = price;
+      })
+      .catch((err) => console.log(err));
+  } else {
+    subtitle.innerText = "Add new product";
+  }
 };
 
 const handleSubmit = (event) => {
@@ -17,7 +62,7 @@ const handleSubmit = (event) => {
   };
 
   fetch(URL, {
-    method: "POST",
+    method: met,
     body: JSON.stringify(newProduct),
     headers: {
       "Content-Type": "application/json",
@@ -33,9 +78,37 @@ const handleSubmit = (event) => {
       }
     })
     .then((createdProduct) => {
-      alert("Product with id: " + createdProduct._id + " created");
-
-      event.target.reset();
+      if (id) {
+        alert("Product with id: " + createdProduct._id + " edited");
+      } else {
+        alert("Product with id: " + createdProduct._id + " created");
+        event.target.reset();
+      }
     })
     .catch((err) => console.log(err));
+};
+const handleDelete = () => {
+  const confirmed = confirm("Do you really want to delete this product?");
+
+  if (confirmed) {
+    fetch(URL, {
+      method: "DELETE",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZTAzYjdmMzA0NjAwMWFlNTlmNTciLCJpYXQiOjE3MTI5MDYyOTksImV4cCI6MTcxNDExNTg5OX0._AqNCIr-c6jwkia_xiX-dt4Xe8vpunRnzSRUA6lp_E0",
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          throw new Error("Fetch error");
+        }
+      })
+      .then((deletedProduct) => {
+        alert("Product: " + deletedProduct.name + " deleted");
+        window.location.assign("./index.html");
+      })
+      .catch((err) => console.log(err));
+  }
 };
